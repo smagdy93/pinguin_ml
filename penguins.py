b8122f6e6a0797c779167956aa91b7bb0a17aa62
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-07 14:42:04 +0200

    Update penguins.py

--- penguins.py
+++ penguins.py
@@ -6,7 +6,7 @@ st.write("This app uses 6 inputs to predict the species of penguin using " "a mo
 penguin_df = pd.read_csv('penguins.csv')
 rf_pickle = open('random_forest_penguin.pickle', 'rb')
 #map_pickle = open('output_penguin.pickle', 'rb')
-#rfc = pickle.load(rf_pickle)
+rfc = pickle.load(rf_pickle)
 #unique_penguin_mapping = pickle.load(map_pickle)
 rf_pickle.close()
 #map_pickle.close()
