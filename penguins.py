a405ab954e4342ae93543152460ebb8dc6967be0
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-08 00:25:46 +0300

    Update penguins.py

--- penguins.py
+++ penguins.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-st.title('Penguin Classifier: A Machine Learning App')
+st.title('Penguin Safaa Classifier: A Machine Learning App')
 st.write("This app uses 6 inputs to predict the species of penguin using " "a model built on the Palmer's Penguin's dataset. Use the form below" " to get started!")
 penguin_df = pd.read_csv('penguins.csv')
 rf_pickle = open('./random_forest_penguin.pickle', 'rb')
